89d62b95971df3be144c24887123d91890b0c6f1
Sierra <iamsi2@protonmail.com>
2020-04-23 11:41:10 +0300

    fixes to env variables

--- docker/elixir/docker-entrypoint.sh
+++ docker/elixir/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # fail if we error out
-set -e
+set "MIX_ENV=dev" && mix compile
 # Wait for database to come up
 # until psql -h db -U "postgres" -c '\q' 2>/dev/null; do
 #   >&2 echo "Postgres is unavailable - sleeping"
@@ -9,6 +9,7 @@ set -e
 # Ensure we have the basics...
 mix local.hex --force
 mix local.rebar --force
+mix deps.get
 mix ecto.create
 mix ecto.migrate
 mix phx.server
